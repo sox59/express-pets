146ad7d27b552e9b3aa4476cc5d34653ef347f38
Paul <ppagliughi@yahoo.com>
2025-04-26 07:27:42 -0500

    form validation

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,9 +1,51 @@
-exports.submitContact = function (req, res) {
 
-  console.log(" From our controller!")
-  console.log(req.body)
+const { ObjectId } = require("mongodb")
+const sanitizeHtml = require("sanitize-html")
+const petsCollection = require("../db").db().collection("pets")
+
+
+const sanitizeOptions = {
+  allowedTags: [],
+  allowedAttributes: {}
+}
+
+
+exports.submitContact = async function (req, res) {
+
+
+  if (req.body.secret.toUpperCase() !== "PUPPY") {
+
+    console.log("Spam Detected")
+    return res.json({ message: "Sorry!" })
+  }
+
+  if (!ObjectId.isValid(req.body.petId)) {
+
+    console.log("Invalid ID Detected")
+    return res.json({ message: "Sorry!" })
+
+  }
+
+  const doesPetExist = await petsCollection.findOne({ _id: new ObjectId(req.body.petId) })
+
+  if (!doesPetExist) {
+
+    console.log("Pet does not exist")
+    return res.json({ message: "Sorry!" })
+
+  }
+
+
+  const ourObject = {
+
+    name: sanitizeHtml(req.body.name, sanitizeOptions),
+    email: sanitizeHtml(req.body.email, sanitizeOptions),
+    comment: sanitizeHtml(req.body.comment, sanitizeOptions)
+
+  }
+
+  console.log(ourObject)
   res.send("Thanks for the data")
-
 
 
 }
